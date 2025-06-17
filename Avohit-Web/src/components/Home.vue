bee61719f1b8a8b83ac79d9017b5560bb5fd2b14
Abril-Ti <abril.tisistemas@grupoavohitdemexico.com>
2025-06-17 09:55:47 -0600

    correccion de vista movil

--- Avohit-Web/src/components/Home.vue
+++ Avohit-Web/src/components/Home.vue
@@ -40,8 +40,9 @@
       <p class="txt-description mb-10">
         ¡Conoce parte de nuestras instalaciones y lo que hacemos para entregarte la mejor calidad!
       </p>
-
-   <SliderComponent/>
+<!-- <SliderComponent/>
+      -->
+   
     </div>
    
     
